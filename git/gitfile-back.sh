2772f4dff94836caa86d00ae05b3c4c068c37169
post <weixiaoyiri@gmail.com>
2016-10-24 19:18:29 +0800

    工作中用到的版本控制方法，把需要管理的文件备份到当前项目的gitfile目录下，使用git做版本记录，并打包备份。这样即使由于意外导致源文件和gitfile目录损坏，也还可以通过压缩文件恢复

---
+++ git/gitfile-back.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd ..
+for i in `ls`
+do
+    filename=`basename $i`
+    extension="${filename##*.}"
+    case $extension in
+        cpp);&
+        c);&
+        h);&
+        rc);&
+		inf);&
+		sh);&
+		py);&
+        txt);&
+        makefile);&
+        SOURCES)
+            echo "backup $i"
+            cp $i gitfile/
+            ;;
+    esac
+done
+
+echo "backup tar.gz"
+tar zcvf gitfile.tar.gz gitfile > /dev/null
