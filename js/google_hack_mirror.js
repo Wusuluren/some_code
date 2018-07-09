9cbb1e641a3bc5ecf3d6c71d322990ed48f1c483
hwave <1634636348@qq.com>
2018-07-09 23:18:46 +0800

    Create google_hack_mirror.js

---
+++ js/google_hack_mirror.js
@@ -0,0 +1,2 @@
+//http://www.hi-ourlife.com/google_hack.html
+document.getElementById('content').innerHTML=document.getElementById('content').innerHTML.replace(/www.google.com/g, 'google.uulucky.com')
