0ba59b9a0c07171f4795f0c110a6775f97d894b7
hwave <1634636348@qq.com>
2018-07-10 22:53:30 +0800

    Delete google_hack_mirror.js

--- js/google_hack_mirror.js
+++
@@ -1,2 +0,0 @@
-//http://www.hi-ourlife.com/google_hack.html
-document.getElementById('content').innerHTML=document.getElementById('content').innerHTML.replace(/www.google.com/g, 'google.uulucky.com')
